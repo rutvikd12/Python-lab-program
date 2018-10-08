0ad0553c3d8d92d3d913c2f3c24dfdec1956ac14
rutvikd12 <43951147+rutvikd12@users.noreply.github.com>
2018-10-08 14:06:09 +0530

    Add files via upload

---
+++ temp1.py
@@ -0,0 +1,8 @@
+x=int(input())
+if x>0:
+ output=1
+ while x>1:
+      output=output*x
+      x=x-1
+print(output)
+
---
+++ temp2.py
@@ -0,0 +1,7 @@
+sum=0
+t=n
+ while (t>0):
+  d=t%10
+  sum+=d**3
+  t=t//10
+return sum
